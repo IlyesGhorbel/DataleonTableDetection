90cf12a5e8db435585154d3ae0b861e42d46bd6a
IlyesGhorbel <ilyesghorbel30@gmail.com>
2025-08-26 22:58:18 +0200

    Fix run.py to use image path from command line arguments

--- run.py
+++ run.py
@@ -1,11 +1,26 @@
 from src.TableDetector import TableDetector
+import argparse
+
+# Créer le parser pour récupérer les arguments
+parser = argparse.ArgumentParser(description="Run table detection on an image")
+parser.add_argument(
+    "--image", type=str, required=True, help="Path to the image you want to analyze"
+)
+parser.add_argument(
+    "--threshold", type=float, default=0.9, help="Confidence threshold (default=0.9)"
+)
+args = parser.parse_args()
 
 # Initialisation du détecteur
 detector = TableDetector(model_name="TahaDouaji/detr-doc-table-detection")
 
-# Lancer la prédiction sur une image d’exemple
-results = detector.predict("Tests/Data/Invoice.png", threshold=0.9)
+# Lancer la prédiction sur l’image passée en argument
+results = detector.predict(args.image, threshold=args.threshold)
 
 # Afficher les résultats
 for r in results:
     print(r)
+
+
+
+
